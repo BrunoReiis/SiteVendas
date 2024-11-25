1a13f2352fdbc169620da5554d18dffb405ceb55
Bruno Reis <bruno.reiis15@gmail.com>
2024-11-25 13:55:10 -0300

    fix: add slides templates

--- config/slideHome.ts
+++ config/slideHome.ts
@@ -1,22 +1,22 @@
 export type SlideHomeConfig = typeof slideHomeConfig;
-import Bruno from '../public/cardsImgs/Bruno.jpeg'
-import Celeste from '../public/cardsImgs/Celeste.jpg'
-import Ga from '../public/cardsImgs/Ga.jpg'
+import Slide1 from '../public/bannerSlides/1.png'
+import Slide2 from '../public/bannerSlides/2.png'
+import Slide3 from '../public/bannerSlides/3.png'
 
 
 export const slideHomeConfig = {
     Slides:[
         {
-            alt: 'Bruno Reis',
-            img: Bruno.src,
+            alt: 'Slide Frete',
+            img: Slide1.src,
         },
         {
-            alt: 'Celeste',
-            img: Celeste.src,
+            alt: 'Moda Feminina',
+            img: Slide2.src,
         },
         {
-            alt: 'Ga',
-            img: Ga.src,
+            alt: 'Black Friday',
+            img: Slide3.src,
         },
     ],
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -37,9 +37,9 @@ export default function IndexPage() {
       return "flex flex-col md:flex-row ml-2 mr-2 space-x-0 md:space-x-4";
     }
     if (screenWidth <= 1500) {
-      return "flex flex-col md:flex-row ml-2 mr-2 mt-4 space-x-0 md:space-x-4";  // Reduzir a margem superior
+      return "flex flex-col md:flex-row ml-2 mr-2 mt-4 space-x-0 md:space-x-4"; 
     } else {
-      return "flex flex-col md:flex-row ml-2 mr-2 mt-0 space-x-0 md:space-x-4";  // Sem margem superior
+      return "flex flex-col md:flex-row ml-2 mr-2 mt-0 space-x-0 md:space-x-4";  
     }
   };
 
@@ -48,14 +48,14 @@ export default function IndexPage() {
       <section className={`${sectionClassNames()} animate-fade-up`}>
         <div className="w-full md:w-5/5">
           {/* Slider Container */}
-          <div className="relative w-full h-[300px] md:h-[150px] overflow-hidden">
+          <div className="relative aspect-video w-full h-[1440px] md:h-[150px] overflow-hidden border-4 border-black-500/75">
             <div className="flex w-full transition-transform duration-500 ease-in-out">
               <div className="flex-shrink-0 w-full h-full" key={slideHomeConfig.Slides[currentSlide].alt}>
                 <Image
                   src={slideHomeConfig.Slides[currentSlide].img}
                   alt={slideHomeConfig.Slides[currentSlide].alt}
                   layout="fill" 
-                  objectFit="cover" 
+                  objectFit="fill" 
                   priority={true} 
                 />
               </div>
