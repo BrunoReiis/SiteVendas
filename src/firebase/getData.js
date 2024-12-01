4279e89907a35d5cbcaea1124e8935b52f06d02f
Bruno Reis <bruno.reiis15@gmail.com>
2024-12-01 17:49:01 -0300

    fix: made a TryCatch for getUserType and return when no user

--- src/firebase/getData.js
+++ src/firebase/getData.js
@@ -1,15 +1,25 @@
-import { collection, query, where, getDoc, doc } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
 import { auth, db } from "./authentication";
 
 export const getUserType = async () => {
   const user = auth.currentUser;
-  const docRef = doc(db, "users", user.uid);
-  const docSnap = await getDoc(docRef);
 
-  if (!docSnap.exists()) {
-      throw new Error("Documento não encontrado.");
+  if (!user) {
+    return null; 
   }
 
-  const userData = docSnap.data();
-  return userData.tipo;
+  try {
+    const docRef = doc(db, "users", user.uid);
+    const docSnap = await getDoc(docRef);
+
+    if (!docSnap.exists()) {
+      throw new Error("Documento não encontrado.");
+    }
+
+    const userData = docSnap.data();
+    return userData.tipo;
+  } catch (error) {
+    console.error("Erro ao obter o tipo de usuário:", error.message);
+    throw new Error("Erro ao buscar os dados do usuário."); 
+  }
 };
